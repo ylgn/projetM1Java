78811ee67b89c8e137fdcdf01213a669e418dca8
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2020-02-26 22:56:39 +0100

    Adding Feature F_40 relatives to Reader (configuration & data) & Mapper pattern

--- M1_MIAGE_POO/src/main/java/mappers/AnonymisationRuleMapper.java
+++ M1_MIAGE_POO/src/main/java/mappers/AnonymisationRuleMapper.java
@@ -4,13 +4,25 @@ import anonymisation.AllAnonymisationRules;
 import meta_data.LineMetaData;
 
 public class AnonymisationRuleMapper {
-	
-	public static String doMap(String type,String toAnon) {
-		//Not Yet implemented
-				return null;
+
+	public static String doMap(String type, String toAnon) {
+		if (type == null) {
+			return toAnon;
+		}
+		switch (type) {
+		case "RANDOM_LETTER":
+			return AllAnonymisationRules.anonymiseFull(toAnon);
+		case "RANDOM_LETTER_FOR_LOCAL_PART":
+			return AllAnonymisationRules.anonymiseForLocalPart(toAnon);
+		default:
+			return toAnon;
+		}
 	}
-	public static String[] lineAnonymisation(LineMetaData lineRef,String[] line) {
-		//Not Yet implemented
-		return null;
+
+	public static String[] lineAnonymisation(LineMetaData lineRef, String[] line) {
+		for (int i = 0; i < line.length; i++) {
+			line[i] = doMap(lineRef.get(i).AnonymisationType, line[i]);
+		}
+		return line;
 	}
 }
--- M1_MIAGE_POO/src/main/java/mappers/DescTypeMapper.java
+++ M1_MIAGE_POO/src/main/java/mappers/DescTypeMapper.java
@@ -4,13 +4,23 @@ import meta_data.LineMetaData;
 import verification.AllVerificationRules;
 
 public class DescTypeMapper {
-	public static boolean doMap(String str,String toCHeck) {
-		//Not Yet implemented
-		return false;
+	public static boolean doMap(String str, String toCHeck) {
+		switch (str) {
+		case "INT":
+			;
+			return AllVerificationRules.checkIfNumber(toCHeck);
+		case "STRING":
+			return AllVerificationRules.checkIfNotNumber(toCHeck);
+		default:
+			return true;
+		}
 	}
-	public static boolean verificationMatchWithDesc(LineMetaData lineRef,String[] line) {
-		//Not Yet implemented
-		return false;
 
+	public static boolean verificationMatchWithDesc(LineMetaData lineRef, String[] line) {
+		boolean temp = true;
+		for (int i = 0; i < line.length; i++) {
+			temp = temp && lineRef.get(i).doDescCHeck(line[i]);
+		}
+		return temp;
 	}
 }
