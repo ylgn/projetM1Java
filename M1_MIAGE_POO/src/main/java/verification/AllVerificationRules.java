9cff381ce18cf844b1a180cba866e5508246c9e1
NadirOmega <nadjemnadir47@gmail.com>
2020-02-27 21:21:05 +0100

    Adding Feature F_50 relatives to Data Verification(Type & rule checking)  -To do :  	finish tests

--- M1_MIAGE_POO/src/main/java/verification/AllVerificationRules.java
+++ M1_MIAGE_POO/src/main/java/verification/AllVerificationRules.java
@@ -1,31 +1,85 @@
 package verification;
 
+/**
+ * Class relative to Verification rules. Here is all method use to verify a
+ * given string
+ * 
+ * @version 1.0
+ * @author Nadir Omega
+ * @author ylgn
+ */
 public class AllVerificationRules {
 
+	/**
+	 * Return true whether the string given is a number or false whether not. To
+	 * manage it we parse an int thanks to Integer.parseInt(String str)
+	 * 
+	 * @param String giving the potential number.
+	 * @return Boolean true if number
+	 */
 	public static boolean checkIfNumber(String str) {
 
-		//Not Yet implemented
-		return false;
+		try {
+			Integer.parseInt(str);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
 
 	}
-	
+
+	/**
+	 * Return true whether the string given is not a number or false whether not. To
+	 * manage it we parse an Double thanks to Double.parseDouble(String str); We
+	 * eliminates leading and trailing spaces with String.trim() onto our String;
+	 * 
+	 * @param String giving the potential number.
+	 * @return Boolean true if not number
+	 */
 	public static boolean checkIfNotNumber(String str) {
-		//Not Yet implemented
+		try {
+			Double.parseDouble(str.trim());
+		} catch (NumberFormatException e) {
+			return true;
+		}
 		return false;
 	}
-	
+
+	/**
+	 * Return true whether the string given is a valid email or false whether not.
+	 * To manage it we check it with regex.
+	 * 
+	 * @param String giving the email under check test.
+	 * @return Boolean true if valid adress
+	 */
 	public static boolean isValidEmail(String email) {
-		//Not Yet implemented
-		return false;
+		String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		return email.matches(regex);
 	}
 
+	/**
+	 * Return true whether the string given is a valid dauphine email or false
+	 * whether not. To manage it we check it with regex.
+	 * 
+	 * @param String giving the email under check dauphine test
+	 * @return Boolean true if valid dauphine adress
+	 */
 	public static boolean isValidDauphineEmail(String email) {
-		//Not Yet implemented
-		return false;
+		String tempString = email.toLowerCase();
+		String check = "^[a-zA-Z0-9_.+-]+@(dauphine\\.eu|dauphine\\.psl\\.eu|lamsade\\.dauphine\\.fr)$";
+		return (tempString.matches(check));
 	}
 
+	/**
+	 * Return true whether the string given is a age or false whether not. To manage
+	 * it we parse an int thanks to Integer.parseInt(String str) and then we check
+	 * if the age is between 0 & 120 years
+	 * 
+	 * @param String giving the potential age.
+	 * @return Boolean true if the string is an adress
+	 */
 	public static boolean isAnAge(String age) {
-		//Not Yet implemented
-		return false;
+		int i = Integer.parseInt(age.trim());
+		return (i > 0 && i < 120);
 	}
 }
--- M1_MIAGE_POO/src/main/java/verification/Verification.java
+++ M1_MIAGE_POO/src/main/java/verification/Verification.java
@@ -1,12 +1,31 @@
 package verification;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import Launcher.MainPg;
+import mappers.DescTypeMapper;
+import mappers.VerificationRuleMapper;
+import meta_data.LineMetaData;
+import readers.cfg_reader.CfgReader;
+import readers.cfg_reader.CfgReaderFactory;
+import readers.document_reader.DocumentReader;
+import readers.document_reader.DocumentReaderFactory;
+import writers.Writer;
+import writers.WriterFactory;
 
+/**
+ * Class relative to Verification process
+ * 
+ * @version 1.0
+ * @author Nadir Omega
+ * @author ylgn
+ */
 public class Verification {
 
-	String in,out,descFilePath,checkFilePath;
-	String separatorIn=";";
-	String separatorOut=";";
-
+	String in, out, descFilePath, checkFilePath;
+	String separatorIn = ";";
+	String separatorOut = ";";
 
 	public Verification(String in, String out, String descFilePath, String anonFilePath) {
 		this.in = in;
@@ -14,8 +33,30 @@ public class Verification {
 		this.descFilePath = descFilePath;
 		this.checkFilePath = anonFilePath;
 	}
+
+	/**
+	 * Run the verification process. First of all, there is an initialisation phase
+	 * with initialisation of the LineMetaData (list of column). Then, as long as it
+	 * will have data to read the program call several method from package mapper in
+	 * order to call method from verification
+	 * 
+	 * @exception Exception
+	 */
 	public void doVerification() throws Exception {
-		//Not Yet implemented
-		
+		DocumentReader csvReader = DocumentReaderFactory.getDocumentReader("CSV", in);
+		Writer csvWriter = WriterFactory.getDocumentWriter("CSV", out);
+		CfgReader descReader = CfgReaderFactory.getCfgReader("JSON", descFilePath);
+		CfgReader checkRuleReader = CfgReaderFactory.getCfgReader("JSON", checkFilePath);
+		LineMetaData lineInit = descReader.initMetaData();
+		LineMetaData lineRef = (checkRuleReader.initRuleMetaData(lineInit));
+
+		while (csvReader.checkContainingData() == true) {
+			ArrayList<String[]> tempLine = csvReader.readMultipleLine(MainPg.blockSize);
+			List<String[]> beforWrite = (tempLine.stream().filter((e) -> {
+				return DescTypeMapper.verificationMatchWithDesc(lineRef, e)
+						&& VerificationRuleMapper.verificationMatchWithRule(lineRef, e);
+			})).collect(Collectors.toList());
+			csvWriter.writeFileFromList(beforWrite);
+		}
 	}
 }
