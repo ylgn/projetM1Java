168cb1aa85f5f1f50cf253498fa96e9744b05f2c
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2020-02-28 23:04:12 +0100

    Adding Feature F_60 relatives to Data Anonymisation

--- M1_MIAGE_POO/src/main/java/anonymisation/AllAnonymisationRules.java
+++ M1_MIAGE_POO/src/main/java/anonymisation/AllAnonymisationRules.java
@@ -1,18 +1,53 @@
 package anonymisation;
 
+import java.util.Random;
+
+/**
+ * Class relative to Anonymisation rules. Here is all method use to verify a
+ * given string
+ * 
+ * @version 1.0
+ * @author Nadir Omega
+ * @author ylgn
+ */
 public class AllAnonymisationRules {
-	
+
 	private static final String alphabet = "abcdefghijklmnopqrstuvwxy";
-	
-	public static String anonymiseForLocalPart(String str) {
-		return null;
-	}
-	
+
 	/**
-	  * @param Str, un string re présentant un nom
-	  * @return un string dont chaque lettre est remplacée par une autre lettre aléatoire
-	*/
+	 * Return an e-mail encrypted with all char before the @
+	 * 
+	 * @param String giving the e-mail
+	 * @return String that contains the anonymised partial address
+	 */
+	public static String anonymiseForLocalPart(String str) {
+		String anonymisedString = "";
+		Random r = new Random();
+		for (int i = 0; i < str.length(); i++) {
+			if (String.valueOf(str.charAt(i)).equals("@")) {
+				while (i < str.length()) {
+					anonymisedString += str.charAt(i);
+					i++;
+				}
+				break;
+			}
+			anonymisedString += alphabet.charAt(r.nextInt(alphabet.length()));
+		}
+		return anonymisedString;
+	}
+
+	/**
+	 * Return a String encrypted entirely
+	 * 
+	 * @param String giving the data to encrypt
+	 * @return String that contains the anonymised string
+	 */
 	public static String anonymiseFull(String str) {
-		return null;
+		String anonymisedString = "";
+		Random r = new Random();
+		for (int i = 0; i < str.length(); i++) {
+			anonymisedString += alphabet.charAt(r.nextInt(alphabet.length()));
+		}
+		return anonymisedString;
 	}
 }
--- M1_MIAGE_POO/src/main/java/anonymisation/Anonymisation.java
+++ M1_MIAGE_POO/src/main/java/anonymisation/Anonymisation.java
@@ -1,17 +1,77 @@
 package anonymisation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import Launcher.MainPg;
+import mappers.AnonymisationRuleMapper;
+import mappers.DescTypeMapper;
+import meta_data.LineMetaData;
+import readers.cfg_reader.CfgReader;
+import readers.cfg_reader.CfgReaderFactory;
+import readers.document_reader.DocumentReader;
+import readers.document_reader.DocumentReaderFactory;
+import writers.Writer;
+import writers.WriterFactory;
+
+/**
+ * Class relative to Anonymisation process
+ * 
+ * @version 1.0
+ * @author Nadir Omega
+ * @author ylgn
+ */
 public class Anonymisation {
-	String in,out,descFilePath,anonFilePath;
-	String separatorIn=";";
-	String separatorOut=";";
+	String in, out, descFilePath, anonFilePath;
+	String separatorIn = ";";
+	String separatorOut = ";";
 
 	public Anonymisation(String in, String out, String descFilePath, String anonFilePath) {
-
+		this.in = in;
+		this.out = out;
+		this.descFilePath = descFilePath;
+		this.anonFilePath = anonFilePath;
 	}
+
+	/**
+	 * Run the anonymisation process. First of all, there is an initialisation phase
+	 * with initialisation of the LineMetaData (list of column). Then, as long as it
+	 * will have data to read the program call several method from package mapper in
+	 * order to call method from anonymisation.
+	 * 
+	 * @exception Exception
+	 */
 	public void doAnonymisation() throws Exception {
+		DocumentReader csvReader = DocumentReaderFactory.getDocumentReader("CSV", this.in);
+		Writer csvWriter = WriterFactory.getDocumentWriter("CSV", out);
+		CfgReader descReader = CfgReaderFactory.getCfgReader("JSON", descFilePath);
+		CfgReader anonRuleReader = CfgReaderFactory.getCfgReader("JSON", anonFilePath);
+
+		LineMetaData lineInit = descReader.initMetaData();
+
+		LineMetaData lineRef = anonRuleReader.initAnonymisationMetaData(lineInit);
+
+		while (csvReader.checkContainingData() == true) {
+			ArrayList<String[]> tempLine = csvReader.readMultipleLine(MainPg.blockSize);
+
+			List<String[]> beforWrite = tempLine.stream().filter((e) -> {
+				return DescTypeMapper.verificationMatchWithDesc(lineRef, e);
+			}).map((e) -> {
+				return AnonymisationRuleMapper.lineAnonymisation(lineRef, e);
+			}).collect(Collectors.toList());
+
+			// beforWrite.stream().map((e)-> {
+			// return AnonymisationRuleMapper.lineAnonymisation(lineRef,e);
+			// }).collect(Collectors.toList());
+
+			/* old version */
+			// for(int i=0;i<beforWrite.size();i++) {
+			// beforWrite.set(i,
+			// AnonymisationRuleMapper.lineAnonymisation(lineRef,beforWrite.get(i)));
+			// }
+			csvWriter.writeFileFromList(beforWrite);
+		}
 
 	}
-
 }
-
